77aeed474d4bcbf4aab8fbe0141410e5ad00656d
ksudarsh <krishna.sudarshan@gmail.com>
2025-03-06 22:56:44 -0500

    Check for "John Doe" same as "john doe" same as "John doe"

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.post("/submit", async (req, res) => {
   try {
     const response = await axios.post(
-      "https://script.google.com/macros/s/AKfycbzuU4RuQBBLIAv93zBB9EOn93oHcQqHDb64tt2CDk5jc50ajvZ6N_3iC0aSXCVzRdxK/exec", // Replace this with the Google Apps Script URL you copied
+      "https://script.google.com/macros/s/AKfycbzfjsHAwpN4OcnveDWk8RqWrdtC_Dy0ZPYJXou1mJ31bs813WbdBO63k98LNU3cJlKR/exec", // Replace this with the Google Apps Script URL you copied
       req.body
     );
     res.json(response.data);
